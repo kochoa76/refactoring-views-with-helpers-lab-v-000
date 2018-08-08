77e4266acad7ca8f1491446818d84f3fc80df14d
Kayla Ochoa <ochoa.kayla@yahoo.com>
2018-08-08 15:35:23 +0000

    automatically backed up by learn

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,3 +1,12 @@
 class Artist < ActiveRecord::Base
   has_many :songs
+
+  def artist_name=(artist)
+    artist.name = song.artist.name 
+  end 
+
+  def artist_name 
+    self.name 
+  end 
+  
 end
