1e162de69eaf3d12cfcf3520d9ff685aaf0c9f91
Kayla Ochoa <ochoa.kayla@yahoo.com>
2018-08-08 15:46:43 +0000

    automatically backed up by learn

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -1,2 +1,11 @@
 module ArtistsHelper
+
+  def display_artist(artist)
+    if artist.name.exists?
+      artist_path(artist)
+    else 
+      link_to "Add Artist", render 'songs/edit'
+    end 
+  end 
+  
 end
