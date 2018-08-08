7d0bcb8dd3647892aba7b6ef4ce2ba5ad63b8361
Kayla Ochoa <ochoa.kayla@yahoo.com>
2018-08-08 16:30:59 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,11 +2,12 @@ class Song < ActiveRecord::Base
   belongs_to :artist
 
   def artist_name
-    self.artist.name if artist 
+    self.artist.name if artist
   end
 
   def artist_name=(artist_name)
-    self.name = name
+    a= Artist.find_or_create_by(artist_name)
+    self.artist = a 
   end
 
 
